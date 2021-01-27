bf85f3eb9dc78a56fcf2d04777e1a879d1ac1483
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-01-27 13:54:33 -0300

    Creo Homvideo-Object solo en index

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 import AnimatedNavbar from './modules/AnimatedNavbar.js';
 import AutoVideo from './modules/AutoVideo.js';
 
+function isIndex() {
+    return window.location.pathname === '/' ? true : false;
+}
+
+/* Main */
 let nav = new AnimatedNavbar('myNav','openNav', 'myLogo');
-let homeVideo = new AutoVideo('homeVideo').loopOn();
+
+if (isIndex()) {
+    let homeVideo = new AutoVideo('homeVideo').loopOn();
+}
