39b9d6de827efd1737c2816babef1c7116a486c9
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-01-28 19:43:14 -0300

    Cambio icono de cierre de navbar

--- assets/js/modules/AnimatedNavbar.js
+++ assets/js/modules/AnimatedNavbar.js
@@ -40,7 +40,7 @@ export default class AnimatedNavbar {
         this.button.innerHTML = '<i class="fas fa-bars"></i><span class="pl-3 fadein d-none d-md-inline">Menu</span>';
     }
     changeButtonToClose () {
-        this.button.innerHTML = '<i class="fas fa-times text-white"></i><span class="pl-3 text-white fadein d-none d-md-inline">Close</span>';
+        this.button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg><span class="pl-3 text-white fadein d-none d-md-inline">Close</span>';
     }
 
 }
