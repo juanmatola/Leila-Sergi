9847974049b97770b2b32904abb5c8ed6b6b5bdd
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-03-04 17:50:38 -0300

    Elimino .jd de control de video y manejo todo con atributos de html5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 import AnimatedNavbar from './modules/AnimatedNavbar.js';
-import AutoVideo from './modules/AutoVideo.js';
 
 function isIndex() {
     return window.location.pathname === '/' ? true : false;
@@ -7,7 +6,3 @@ function isIndex() {
 
 /* Main */
 let nav = new AnimatedNavbar('myNav','openNav', 'myLogo');
-
-if (isIndex()) {
-    let homeVideo = new AutoVideo('homeVideo').loopOn();
-}
