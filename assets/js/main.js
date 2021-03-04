3447a493b7549d1fc8e4b5d9a2341274e7b441ca
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-03-04 18:08:53 -0300

    Elimino .js no utilizado y su llamada desde el modulo main (AutoVideo module)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
 import AnimatedNavbar from './modules/AnimatedNavbar.js';
 
-function isIndex() {
-    return window.location.pathname === '/' ? true : false;
-}
-
-/* Main */
 let nav = new AnimatedNavbar('myNav','openNav', 'myLogo');
