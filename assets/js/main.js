45fc88a449114e968353b7751344c6dd60dcd328
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-03-30 12:33:01 -0300

    Soluciono error de visualizacion de floatWorkButton en navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 import AnimatedNavbar from './modules/AnimatedNavbar.js';
 
 let nav = new AnimatedNavbar('myNav','openNav', 'myLogo');
+
+let navButton = document.getElementById('openNav');
+let floatWorkButton = document.getElementById('floatWorkButton');
+
+navButton.addEventListener('click',()=>{
+    if(nav.status === 'open'){
+        floatWorkButton.style.opacity = '0';
+    }else{
+        floatWorkButton.style.opacity = '1';
+    }
+});
